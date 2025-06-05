cc3d9002d6f13b4583998a7fa9454495240ab01c
cxoptimized <cxoptimized@gmail.com>
2025-06-05 12:30:35 -0400

    Delete submit.js

--- submit.js
+++
@@ -1,9 +0,0 @@
-export default async function handler(req, res) {
-  if (req.method === 'POST') {
-    const data = req.body;
-    // Process submission logic here
-    res.status(200).json({ message: 'Submission received', data });
-  } else {
-    res.status(405).json({ message: 'Method Not Allowed' });
-  }
-}
